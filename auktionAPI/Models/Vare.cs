4333fe6290bb1b32175ee55f468d2f7a21c09cfc
AndreasHp60 <AndreasHp60@live.com>
2022-11-25 09:49:49 +0100

    auktionsAPI og kundeAPI GET 1.2

---
+++ auktionAPI/Models/Vare.cs
@@ -0,0 +1,12 @@
+namespace auktionsAPI; 
+ 
+  public class Vare 
+  { 
+      public int Id { get; set; } 
+      public string? Navn { get; set; } 
+ 
+      public string? Beskrivelse { get; set; } 
+ 
+      public double? Vurdering { get; set; } 
+ 
+  } 
