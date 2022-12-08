f798004d017281dcb27c7d5d9ba49fe1156b35e5
MarthTheCreater <82372299+MarthTheCreater@users.noreply.github.com>
2022-12-08 09:05:02 +0100

    vareAPI 2.0.0 (get, post, put delete)

---
+++ vareAPI/Models/Product.cs
@@ -0,0 +1,32 @@
+namespace ProductService; 
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+ 
+  public class Product 
+  { 
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+      public string? Id { get; set; } 
+      [BsonElement("name")]
+      public string? Name { get; set; } 
+      [BsonElement("description")]
+      public string? Description { get; set; } 
+      [BsonElement("category")]
+      public string? Category { get; set; } 
+      [BsonElement("assesment")]
+      public double? Assesment { get; set; } 
+      [BsonElement("price")]
+      public double? Price { get; set; } 
+      [BsonElement("minbid")]
+      public double? MinBid { get; set; } 
+      [BsonElement("time")]
+      public DateTime? Time { get; set; } 
+      [BsonElement("images")]
+      public string? Images { get; set; } 
+      [BsonElement("state")]
+      public short? State { get; set; } 
+
+    [BsonElement("customer")]
+    public Customer? customer {get; set; }     
+ 
+  } 
