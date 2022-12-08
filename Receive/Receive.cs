e57f2f8df68c89f324f75cc7a15dc60117fbe116
AndreasHp60 <AndreasHp60@live.com>
2022-12-08 09:30:31 +0100

    messagebroker 2.1.0

---
+++ Receive/Receive.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Text.Json;
+
+namespace productReceive;
+
+class Receive
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "products",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+
+            channel.BasicConsume(queue: "products",
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Send/Product.cs
@@ -0,0 +1,45 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace productSender;
+ 
+  public class Product 
+  { 
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+      public string? Id { get; set; } 
+      [BsonElement("name")]
+      public string? Name { get; set; } 
+      [BsonElement("description")]
+      public string? Description { get; set; } 
+      [BsonElement("category")]
+      public string? Category { get; set; } 
+      [BsonElement("assesment")]
+      public double? Assesment { get; set; } 
+      [BsonElement("price")]
+      public double? Price { get; set; } 
+      [BsonElement("minbid")]
+      public double? MinBid { get; set; } 
+      [BsonElement("time")]
+      public DateTime? Time { get; set; } 
+      [BsonElement("images")]
+      public string? Images { get; set; } 
+      [BsonElement("state")]
+      public short? State { get; set; } 
+
+      [BsonElement("customer")]
+      public Customer? Customer {get; set;}   
+      public Product(string? name, string? description, string? category, double? assesment, double? price, double? minbid, DateTime? time, string? images, short? state)
+        {
+            name = Name;
+            description = Description;
+            category = Category;
+            assesment = Assesment;
+            price = Price;
+            minbid = MinBid;
+            time = Time;
+            images = Images;
+            state = State;
+        }  
+ 
+  } 
